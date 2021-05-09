359162cb174204949ce5ad620cd193fb23fa2236
thatoranzhevyy <thatoranzhevyy@gmail.com>
2021-05-09 23:32:36 +0600

    add configs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log("tweetsdfkj")
+// Streak graph picture
+const commits: string[] = [
+    /* columns
+	'      10>|      20>|      30>|      40>|      50>|  '   < exactly 52 characters */
+    '                                                    ',  // Sun
+    '                                                    ',  // Mon
+    '                                                    ',  // Tue
+    '                                                    ',  // Wed
+    '                                                    ',  // Thu
+    '                                                    ',  // Fri
+    '                                                    ',  // Sat
+]
+//  *  for 10 commits in that day
+//  #  for 20 commits in that day
+
+// Repository origin
+const repository: string = 'https://github.com/thatoranzhevyy/contribution-designer.git';
+
+console.log("draw graph for repository: " + repository)
